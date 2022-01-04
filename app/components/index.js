49fdba172feabcfdda1823a54a2d2feeb8ca2452
ayush-rathore <aayushh79@gmail.com>
2022-01-04 20:55:34 +0530

    Export default screen

--- app/components/index.js
+++ app/components/index.js
@@ -1,5 +1,6 @@
 import Button from "./Button";
 import ErrorMessage from "./ErrorMessage";
 import InputField from "./InputField";
+import Screen from "./Screen";
 
-export default { Button, ErrorMessage, InputField };
+export default { Button, ErrorMessage, InputField, Screen };
