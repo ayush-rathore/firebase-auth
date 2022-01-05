7ee46046844249c37997d43cabc51196c176f655
ayush-rathore <aayushh79@gmail.com>
2022-01-05 11:41:20 +0530

    Return RootNav with UserProvider

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { UserProvider } from "./app/context/UserContext";
+import RootNav from "./app/navigation/RootNav";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+	return (
+		<UserProvider>
+			<RootNav />
+		</UserProvider>
+	);
+};
+
+export default App;
